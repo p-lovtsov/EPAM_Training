a65229ff3f264a2998d742c4ecd6a62ca35b216d
p-lovtsov <p.lovtsov@gmail.com>
2017-10-24 22:13:22 +0300

    Добавил папку JS_Task_5

--- JS_Task_4/js_task4_11.js
+++
@@ -1,7 +0,0 @@
-/*Написать функцию, которая возвращает случайное число в диапазоне от 0 до 100*/
-
-var rand = function () {
-    return Math.round(Math.random()*101);
-}
-
-console.log(rand());
--- JS_Task_4/js_task4_12.js
+++
@@ -1,7 +0,0 @@
-/*Написать функцию, которая возвращает целое случайное число в диапазоне от min до max*/
-
-var randMinMax = function (min, max) {
-    return Math.round( Math.random() * (max - min) + min );
-}
-
-console.log(randMinMax(0,10));

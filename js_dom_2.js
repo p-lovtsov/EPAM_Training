00c5bb43e2f561dfc08a05637b9057d28ee4c3bd
p-lovtsov <p.lovtsov@gmail.com>
2017-11-30 21:01:55 +0300

    class

---
+++ js_dom_2.js
@@ -0,0 +1,70 @@
+var Truecalc = function () {
+    var state = 0;
+    return {
+        add: function (a) {
+            return state += a;
+        }
+    }
+};
+
+var calc = new Truecalc;
+
+console.log(calc.add(5));
+console.log(calc.add(4));
+
+// var truecalc = function () {
+//     state = 0;
+//     return {
+//         add: function (a) {
+//             state += a;
+//             var func = function (b) {
+//                 return state += b;
+//             }
+//             return func;
+//         },
+//         multiply: function (a) {
+//             state *= a;
+//             var func = function (b) {
+//                 return state *= b;
+//             }
+//             return func;
+//         },
+//         devide: function (a) {
+//             state /= a;
+//             var func = function (b) {
+//                 return state /= b;
+//             }
+//             return func;
+//         },
+//         subtract: function (a) {
+//             state -= a;
+//             var func = function (b) {
+//                 return state -= b;
+//             }
+//             return func;
+//         },
+//         getResult: function () {
+//             return state;
+//         },
+//         reset: function() {
+//             state = 0;
+//             return state;
+//         }
+//     }
+// }
+
+// var calc = truecalc();
+
+// calc.multiply(3)(2);
+// console.log(calc.getResult());
+// calc.add(4)(3);
+// console.log(calc.getResult());
+// calc.add(4);
+// console.log(calc.getResult());
+// calc.reset();
+// console.log(calc.getResult());
+// calc.multiply(3);
+// console.log(calc.getResult());
+// calc.devide(9);
+// calc.subtract(4);
+// console.log(calc.getResult());

7792a5b47a8181efe9b24cdba687130dac0509f5
p-lovtsov <p.lovtsov@gmail.com>
2018-02-23 21:06:48 +0300

    somthing changed

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 var brdName = '';
 var actions = [];
+var ArrayOfLists = [];
 var board;
 var elemBoardName = document.querySelector('#boardName');
 var elemRenameBoard = document.querySelector('#renameBoard');
+var listName = document.querySelectorAll('.listName');
 var addListForm = document.querySelector('.addList');
 var addListInput = document.querySelector('#addList-input');
 var addListSave = document.querySelector('#addList-save');
 var addListCloseBtn = document.querySelector('.close-btn');
 var page = document.querySelector('.container');
-var lists = document.querySelector('.wrap');
+var lists = document.querySelector('.lists');
 
 window.onload = init();
 
@@ -24,7 +26,6 @@ function init () {
     }
     brdName = localStorage.getItem('boardName');
     elemBoardName.innerText = brdName;
-
 }
 
 elemBoardName.addEventListener('click', function() {
@@ -32,7 +33,6 @@ elemBoardName.addEventListener('click', function() {
     var closeBtn = document.querySelector('.modal-btn-close');
     var renameBtn = document.querySelector('#btnRenameBoard');
     inputBoardName.value = elemBoardName.innerText;
-    console.dir(inputBoardName);
     if (modal.classList.contains('hide')) {
         modal.classList.remove('hide');
     };
@@ -61,19 +61,50 @@ addListSave.addEventListener('click', function (event) {
         var divList = document.createElement('div');
         var divListHeader = document.createElement('div');
         var h4 = document.createElement('h4');
+        var renameField = document.createElement('input');
+        var listMenu = document.createElement('div');
+        var cards = document.createElement('div');
+        cards.className = 'cards';
+        listMenu.className = 'list-menu';
+        listMenu.innerText = '...';
+        renameField.className = 'hide';
         divList.className = 'list';
         divListHeader.className = 'list-header';
-
         h4.className = 'listName';
-        h4.innerText = addListInput.value + ' ' + uuid();
+        h4.id = uuid();
+        h4.innerHTML = addListInput.value;
+        ArrayOfLists.push(h4.id);
+        h4.addEventListener('click', function (event) {
+            renameList (event);
+        });
         divListHeader.appendChild(h4);
+        divListHeader.appendChild(renameField);
+        divListHeader.appendChild(listMenu);
         divList.appendChild(divListHeader);
         lists.insertBefore(divList,addListForm);
         addListInput.value = '';
     }
-    console.log('save', event.target.parentNode.parentNode);
-
 });
+
+function renameList (event) {
+    var inp = document.createElement('input');
+    inp.classList.add('renameList');
+    var name = event.target;
+    inp.value = name.innerText;
+    name.appendChild(inp);
+    inp.focus();
+    // inp.addEventListener('focusout', function (event) {
+    //     var parent = event.currentTarget.parentNode;
+    //     parent.removeChild(event.currentTarget);
+    // });
+    inp.addEventListener('keydown', function (event) {
+        if (event.keyCode === 13 && inp.value !== "") {
+            var parent = event.currentTarget.parentNode;
+            parent.innerText = inp.value;
+            parent.removeChild(inp);
+        }
+    });
+}
 
 addListCloseBtn.addEventListener('click', function (event) {
     addListInput.value = '';
@@ -90,11 +121,18 @@ function uuid() {
     return uuid;
 }
 
-addListInput.addEventListener('blur', function (event) {
-    console.log('blur', event.currentTarget, event.target);
-    event.currentTarget.classList.add('hide');
-});
-
-// addListForm.addEventListener('click', function(event) {
-//     console.log(event.currentTarget);
+// addListInput.addEventListener('focusout', function (event) {
+//     console.log(event.currentTarget, event.target);
+//     var addListFooter = document.querySelector('.addList-footer');
+//     addListFooter.classList.add('hide');
 // });
+
+lists.addEventListener('click', function (event) {
+    // console.log(event.target);
+})
+
+addListForm.addEventListener('click', function(event) {
+    if (event.target === addListSave) {
+        console.log(1);
+    }
+});
